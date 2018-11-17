67c0eb0da833177e9f40aae85b01087b1777d283
ecabrerar <eudris@gmail.com>
2018-11-17 07:59:25 -0400

    cambio

--- java-ee-8-microprofile/src/main/java/com/eudriscabrera/examples/java/BarcampRDEndPoint.java
+++ java-ee-8-microprofile/src/main/java/com/eudriscabrera/examples/java/BarcampRDEndPoint.java
@@ -1,45 +1,59 @@
 package com.eudriscabrera.examples.java;
 
-import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import javax.json.Json;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 
 /**
  * @author ecabrerar
  * @date Nov 17, 2018
  */
-@Timed(name="barcamprd", absolute=true,unit=MetricUnits.MILLISECONDS)
+@Timed(name = "barcamprd", absolute = true, unit = MetricUnits.MILLISECONDS)
 @Path("/barcamprd")
 public class BarcampRDEndPoint {
 
 	@GET
+	@Counted
 	@Path("/desconferencias")
 	@Produces("application/json")
 	public Response desconferencias() {
-		return 
-				 Response
-	                .ok(Json.createReader(new StringReader( ClassLoader.getSystemClassLoader().getResourceAsStream("/src/main/resources/META-INF/desconferencias.json").toString())))
-	                .build();
-				
-				
-			
-		
+		return Response.ok(new Recurso().getConferencias()).build();
+
 	}
-	
-	
+
 	@GET
 	@Path("/desconferencistas")
-	@Produces("application/json")
+	@Produces("application/json")	
 	public Response desconferencistas() {
-		return Response
-                .ok("")
-                .build();
+		return Response.ok(new Recurso().getConferencias().stream().map(desconf->desconf.getCharlista()).collect(Collectors.toList())).build();
+	}
+
+	static class Recurso {		
+			
+
+		private List<Desconferencia> getConferencias() {
+			
+			List<Desconferencia> desconferencias = new ArrayList<>();
+					desconferencias.add(new Desconferencia("9:00-10:00 A.M.", "Luis Contreras", "Una imagen vale más mil palabras"));
+			desconferencias.add(new Desconferencia("9:00-10:00 A.M.", "Clivens", "Desarrollar Lenguajes DSL(Domain Specific Language) con Antlr"));
+			desconferencias.add(new Desconferencia("9:00-10:00 A.M.", "Eudris Cabrera", "Desarrollo de aplicaciones orientada a la nube con Java EE 8 / Jakarta EE y Microprofile"));
+			desconferencias.add(new Desconferencia("9:00-10:00 A.M.", "Cindia Espinal", "Gestión de Riesgos de Seguridad de Información"));
+			desconferencias.add(new Desconferencia("10:00-11:00 A.M.", "Jonathan Garrido", "La configuración default es tu enemiga"));
+			desconferencias.add(new Desconferencia("10:00-11:00 A.M.", "Edgar Garcia", "Software Architect: The Journey"));
+			desconferencias.add(new Desconferencia("10:00-11:00 A.M.", "Yhorby Matías", "¿Javascript ha muerto? .NET Web Assembly con Blazor"));
+			desconferencias.add(new Desconferencia("10:00-11:00 A.M.", "Julissa Mateo", "Comunidades tecnológicas para desarrollo profesional"));
+			
+	
+			return desconferencias;
+		}
 	}
 }
--- java-ee-8-microprofile/src/main/java/com/eudriscabrera/examples/java/Desconferencia.java
+++ java-ee-8-microprofile/src/main/java/com/eudriscabrera/examples/java/Desconferencia.java
@@ -9,28 +9,43 @@ package com.eudriscabrera.examples.java;
  */
 public class Desconferencia {
 
+	public Desconferencia(String hora, String charlista, String tema) {
+		super();
+		this.hora = hora;
+		this.charlista = charlista;
+		this.tema = tema;
+	}
+
+	public Desconferencia() {
+
+	}
+
 	private String hora;
 	private String charlista;
 	private String tema;
-	
+
 	public String getHora() {
 		return hora;
 	}
+
 	public void setHora(String hora) {
 		this.hora = hora;
 	}
+
 	public String getCharlista() {
 		return charlista;
 	}
+
 	public void setCharlista(String charlista) {
 		this.charlista = charlista;
 	}
+
 	public String getTema() {
 		return tema;
 	}
+
 	public void setTema(String tema) {
 		this.tema = tema;
 	}
-	
-	
+
 }
